27a9caae1e0392f9a4bdb58cec22a297486edaaa
xavier7778 <xaviersehwag7778@gmail.com>
2022-09-11 11:07:08 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/Header"
+import Form from "./components/Meme"
+
+export default function App(){
+  return(
+    <div>
+      <Header />
+      <Form />
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="../TrollFace.png" alt="trollface" className="header--image"></img>
+            <h2 className="header--title">MEME GENERATOR</h2>
+            <h4 className="header--project">React course-project 3</h4>
+        </header>
+    )
+}
